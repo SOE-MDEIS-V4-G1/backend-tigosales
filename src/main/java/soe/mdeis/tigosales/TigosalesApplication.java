3b4c5722d8ab828c09ce87e4e87d3bc180600fd6
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2024-02-18 11:18:45 -0400

    Configuración Inicial del proyecto

---
+++ src/main/java/soe/mdeis/tigosales/TigosalesApplication.java
@@ -0,0 +1,13 @@
+package soe.mdeis.tigosales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigosalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigosalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soe/mdeis/tigosales/TigosalesApplicationTests.java
@@ -0,0 +1,13 @@
+package soe.mdeis.tigosales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigosalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
