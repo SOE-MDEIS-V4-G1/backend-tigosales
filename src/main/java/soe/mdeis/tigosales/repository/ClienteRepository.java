c8941c9925498045bcb07e9cf0590094c905dbdb
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2024-02-19 14:49:40 -0400

    Creando ClienteRepository

---
+++ src/main/java/soe/mdeis/tigosales/repository/ClienteRepository.java
@@ -0,0 +1,18 @@
+package soe.mdeis.tigosales.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import soe.mdeis.tigosales.model.entity.Cliente;
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByNombre(String nombre);
+
+    boolean existsById(long id);
+
+    Optional<Cliente> findByCi(String ci);
+
+}
