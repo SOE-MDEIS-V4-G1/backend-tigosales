967ed5855258097c3b6ce78a716ddd12d5531000
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2024-02-20 11:55:54 -0400

    Creando Repositorio SucursalRepository

---
+++ src/main/java/soe/mdeis/tigosales/repository/SucursalRepository.java
@@ -0,0 +1,14 @@
+package soe.mdeis.tigosales.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import soe.mdeis.tigosales.model.Sucursal;
+
+public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
+
+    Optional<Sucursal> findOneByNombre(String nombre);
+
+    boolean existsById(Long id);
+}
