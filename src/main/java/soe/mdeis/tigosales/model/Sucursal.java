df74421517a454dda4ff3edc4a61ba321f4113d9
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2024-02-20 11:54:55 -0400

    Creando Entidad Sucursal

---
+++ src/main/java/soe/mdeis/tigosales/model/Sucursal.java
@@ -0,0 +1,35 @@
+package soe.mdeis.tigosales.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sucursales")
+public class Sucursal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(length = 50)
+    public String nombre;
+
+    @Transient
+    public List<Usuario> usuarios;
+
+}
