8cc71ca58b793eb103d4b402e7235419b055e693
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2024-02-20 03:08:51 -0400

    Creando LoginDto

---
+++ src/main/java/soe/mdeis/tigosales/dto/LoginDto.java
@@ -0,0 +1,17 @@
+package soe.mdeis.tigosales.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+
+    private String user;
+
+    private String password;
+}
