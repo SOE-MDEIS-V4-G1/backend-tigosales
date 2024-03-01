81017790fc9c3fc71535c863f0176b4900110815
Gustavo Vargas Miranda <gvmgust@hotmail.com>
2024-03-01 15:26:45 -0400

    Creando entidades venta e itemVendido

---
+++ src/main/java/soe/mdeis/tigosales/model/ItemVendido.java
@@ -0,0 +1,45 @@
+package soe.mdeis.tigosales.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ItemVendido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int cantidad;
+
+    @Column(scale = 2, precision = 10)
+    private BigDecimal precioUnitario;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "venta_id")
+    @JsonIgnore
+    private Venta venta;
+
+}
